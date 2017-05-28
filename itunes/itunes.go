b5d30c345e199edb7a31b4fc5e13bff2e6f798d4
polidog <polidogs@gmail.com>
2017-05-28 10:27:23 +0900

    スクリプト修正

--- itunes/itunes.go
+++ itunes/itunes.go
@@ -8,8 +8,6 @@ import (
 
 func main() {
 	var command string
-	fmt.Println(os.Args[1])
-	fmt.Println(os.Args)
 	if len(os.Args) > 0 {
 		command = os.Args[1]
 	}
@@ -26,7 +24,7 @@ func main() {
 
 	fmt.Println("----------track-----------")
 	fmt.Println("Album:", res.Track.Album)
-	fmt.Println("Album:", res.Track.Name)
+	fmt.Println("Name:", res.Track.Name)
 	fmt.Println("Artist:", res.Track.Artist)
 	fmt.Println("Category:", res.Track.Category)
 	fmt.Println("Time:", res.Track.Time)
